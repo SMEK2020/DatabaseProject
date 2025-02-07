32b85c6583d8a1df64ed20e1ad2b3d6a8d10e2f6
Sazid Mahmud <sazid.bsmru.cse1@gmail.com>
2025-02-07 12:14:18 -0800

    first commit

---
+++ downloadresult.php
@@ -0,0 +1,121 @@
+<?php
+    include('connect.php');
+    session_start();
+    if( !isset($_SESSION['id'])  ){
+        header('location: Slogin.php');
+        exit;
+    }
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download Result</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="update.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    
+
+</head>
+
+    <div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-light border-end" id="sidebar-wrapper">
+            <div class="sidebar-heading text-center py-4 primary-text"> 
+                <img src="image/my pic.png" class="rounded-circle" width="80" alt="Profile Picture">
+                <h6>SAZID MAHMUD EMON KHAN</h6>
+            </div>
+            <div class="list-group list-group-flush">
+                <a href="dashboard.php" class="list-group-item list-group-item-action">Dashboard</a>
+                <a href="updateprofile.php" class="list-group-item list-group-item-action">Update Profile</a>
+                <a href="downloadresult.php" class="list-group-item list-group-item-action  active">Download Result</a>
+                <a href="certificaterequest.php" class="list-group-item list-group-item-action">Certificate Application</a>
+                <a href="#" class="list-group-item list-group-item-action">Change Password</a>
+                <a href="logouthelper.php" class="list-group-item list-group-item-action">Logout</a>
+            </div>
+        </div>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <nav  class="navbar navbar-expand-lg navbar-light bg-white border-bottom px-3">
+                <div class="container-fluid d-flex align-items-center" >
+                    <img src="image/bsmru_logo.png" alt="Logo" width="70" class="me-2">
+                    <h5 class="m-0">Bangabandhu Sheikh Mujibur Rahman University, Kishoreganj</h5>
+                </div>
+            </nav>
+
+            <div class="container mt-4">
+            <div class="container">
+            <h2 class="text-center">Download Result</h2><br><br>
+            <table class="table table-hover">
+            <thead>
+    <tr>
+      <th scope="col">Serial No.</th>
+      <th scope="col">Semester</th>
+      <th scope="col">Published Date</th>
+      <th style="text-align: center;">
+    
+        Download
+    
+</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>1st Year 1st</td>
+      <td>30 November,2023</td>
+      <td style="text-align: center;">
+    <a href="#" style="display: inline-block;">
+        <i class="fa-solid fa-download"></i>
+    </a>
+</td>
+
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>1st Year 2nd</td>
+      <td>24 June,2024</td>
+      <td style="text-align: center;">
+    <a href="#" style="display: inline-block;">
+        <i class="fa-solid fa-download"></i>
+    </a>
+</td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>2nd Year 1st</td>
+      <td >30 January,2025</td>
+      <td style="text-align: center;">
+    <a href="#" style="display: inline-block;">
+        <i class="fa-solid fa-download"></i>
+    </a>
+</td>
+
+    </tr>
+  </tbody>
+            </table>
+            </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Computer Science and Engineering Department</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="image/favicon.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Playfair+Display:wght@700;900&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
+    </div>
+    <!-- Spinner End -->
+
+    
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-white sticky-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-2 py-lg-0">
+                <a href="index.html" class="navbar-brand">
+                    <img class="img-fluid" src="image/favicon.png" alt="Logo">
+                </a>
+                <button type="button" class="navbar-toggler ms-auto me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    
+                    <div class="navbar-nav ms-auto">
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="#" class="nav-item nav-link">About</a>
+                        <a href="#" class="nav-item nav-link">Notice</a>
+                       
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Academic</a>
+                            <div class="dropdown-menu bg-light rounded-0 m-0">
+                                <a href="#" class="dropdown-item">Syllebus</a>
+                                <a href="#" class="dropdown-item">Courses</a>
+                                <a href="#" class="dropdown-item">Question Bank</a>
+                                
+                            </div>
+                        </div>
+                        <a href="#" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <div class="border-start ps-4 d-none d-lg-block">
+                        <button type="button" class="btn btn-sm p-0"><i class="fa fa-search"><a href="##">Validity Check</a></i></button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <div class="back-img" style="background-image: url('image/carousel-1.jpg');background-size: cover;">
+        <marquee><h1>WELCOME TO DEPT. OF COMPUTER SCIENCE AND ENGINEERING</h1></marquee>
+
+    </div>
+    
+
+    <!-- Article Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                    <img class="img-fluid" src="image/bijoy.jpg" alt="">
+                </div>
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="section-title">
+                        <p class="fs-5 fw-medium fst-italic text-primary">Department of </p>
+                        <h1 class="display-6">Computer Science and Engineering</h1>
+                    </div>
+                    <p class="mb-4">But that's not all. The CSE Department also encourages the exciting world of robotics, sparking the imaginations of budding engineers. Moreover, it consistently motivates students to develop practical systems that find real-world applications, bridging the gap between theory and practice.</p>
+                    <p class="mb-4">At Bangabandhu Sheikh Mujibur Rahman University, the CSE Department aims to create an inclusive and supportive community where students can pursue their academic dreams while contributing to the ever-evolving fields of computer science and engineering. Join this exciting journey and be a part of shaping the future!
+
+                    </p>
+                    <p class="mb-4">But that's not all. The CSE Department also encourages the exciting world of robotics, sparking the imaginations of budding engineers. Moreover, it consistently motivates students to develop practical systems that find real-world applications, bridging the gap between theory and practice.
+
+                    </p>
+                    <p class="mb-4">At Bangabandhu Sheikh Mujibur Rahman University, the CSE Department aims to create an inclusive and supportive community where students can pursue their academic dreams while contributing to the ever-evolving fields of computer science and engineering. Join this exciting journey and be a part of shaping the future!
+
+                    </p>
+                    <a href="" class="btn btn-primary rounded-pill py-3 px-5">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Article End -->
+        
+
+   
+
+
+    <!-- Products Start -->
+    <div class="container-fluid product py-5 my-5">
+        <div class="container py-5">
+            <div class="section-title text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                
+                <h1 class="display-6 ">Event Section</h1>
+            </div>
+            <div class="owl-carousel product-carousel wow fadeInUp" data-wow-delay="0.5s">
+                <a href="" class="d-block product-item rounded">
+                    <img src="image/csefest2024.jpg" alt="">
+                    <div class="bg-white shadow-sm text-center p-4 position-relative mt-n5 mx-4">
+                        <h4 class="text-primary">CSE FEST 2024</h4>
+                        <span class="text-body">Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum</span>
+                    </div>
+                </a>
+                <a href="" class="d-block product-item rounded">
+                    <img src="image/ideacon.jpg" alt="">
+                    <div class="bg-white shadow-sm text-center p-4 position-relative mt-n5 mx-4">
+                        <h4 class="text-primary">IDEA CONTEST 2024</h4>
+                        <span class="text-body">Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum</span>
+                    </div>
+                </a>
+                <a href="" class="d-block product-item rounded">
+                    <img src="image/quizcon.jpg" alt="">
+                    <div class="bg-white shadow-sm text-center p-4 position-relative mt-n5 mx-4">
+                        <h4 class="text-primary">QUIZ CONTEST 2024</h4>
+                        <span class="text-body">Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum</span>
+                    </div>
+                </a>
+                <a href="" class="d-block product-item rounded">
+                    <img src="image/idpc.jpg" alt="">
+                    <div class="bg-white shadow-sm text-center p-4 position-relative mt-n5 mx-4">
+                        <h4 class="text-primary">IDPC 2024</h4>
+                        <span class="text-body">Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- Products End -->
+
+
+    
+
+
+     <!-- Team Start -->
+     <div class="container-xxl pt-5 pb-3">
+        <div class="container" >
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h3 class="section-title ff-secondary text-center text-primary fw-normal">Faculty Member</h3>
+                
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/fac2.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Al Amin Biswas</h5>
+                        <small>Chairman</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-4">
+
+                
+                
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/fac1.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Swapnil Biswas</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/fac3.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Noshin Tahsin</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/fac4.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Md. Sabab Zulfiker</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/Md_Kamrul_Hasan_Tuhin.png" alt="">
+                        </div>
+                        <h5 class="mb-0">Md. Kamrul Hasan Tuhin</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/SCR.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Dr. Sujan Chandra Roy</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <div class="rounded-circle overflow-hidden m-4">
+                            <img class="img-fluid" src="image/HAR.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Md. Harun Or Rashid</h5>
+                        <small>Lecturer</small>
+                        <div class="d-flex justify-content-center mt-3">
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- Team End -->
+
+
+
+
+
+
+
+
+    
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-5 col-md-12">
+                    <h4 class="text-primary mb-4">Bangabandhu Sheikh Mujibur Rahman University, Kishoreganj</h4>
+                    <hr>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>3rd floor, Bangabandhu Sheikh Mujibur Rahman Academic Bhaban, Gurudayal Govt. College, Temporary Campus of Bangabandhu Sheikh Mujibur Rahman University, Kishoreganj</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt text-primary me-3"></i>N/A</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i> cse@bsmru.ac.bd</p>
+                </div>
+                <div class="col-lg-7 col-md-12">
+                    <div class="map" style="position: relative; width: 100%; padding-top: 56.25%;"> <!-- 16:9 Aspect Ratio -->
+                        <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d908.060223270886!2d90.7741252448389!3d24.442426277284493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375693bff2e9b0c5%3A0x948309aeb6bfb1d5!2sUniversity%20of%20Kishoreganj!5e0!3m2!1sen!2sbd!4v1737977504390!5m2!1sen!2sbd" 
+                            width="100%" 
+                            height="75%" 
+                            style="border:0; position: absolute; top: 0; left: 0;" 
+                            allowfullscreen="" 
+                            loading="lazy" 
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+            
+            <hr>
+            <div class="row">
+                <div class="log">
+                    <div class=""><a href="Alogin.html" target="#">Admin Login</a></div>
+                <div class=""><a href="Slogin.php" target="#">Student Login</a></div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="fw-medium" href="#">DB Team-5</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    
+                    Designed By <a class="fw-medium" href="#">DB Team-5</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
